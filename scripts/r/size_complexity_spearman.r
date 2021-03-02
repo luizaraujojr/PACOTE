b138ea25e868af10f021d2595bacc7f263e1abd3
luizaraujojr <luiz.araujojr@uniriotec.br>
2021-03-01 21:12:19 -0300

    Revert "."
    This reverts commit 6014f990b0acf37cf6a743c5bc51a266d2b6c5e7.

--- scripts/r/size_complexity_spearman.r
+++ scripts/r/size_complexity_spearman.r
@@ -1,21 +1,6 @@
-	project<-"JEdit"
-	
-	JHotDraw_Exclude <- c("5.4.2");
-	JEdit_Exclude <- c("2.3.2", "2.3.3", "2.3.4", "2.3.5", "2.3.6", "2.3.7", "2.3.f", "2.4.2", "2.5.1", "3.0.1", "3.2.1", "4.0.0", "4.0.2", "4.3.0", "4.3.1", "4.3.2","5.4.0");
-	JEdit_Exclude <- c("2.3.2", "2.3.3", "2.3.4", "2.3.5", "2.3.6", "2.3.7", "2.3.f", "2.4.2", "2.5.1", "3.0.1", "3.2.1", "4.0.0", "4.0.2", "4.3.0", "4.3.1", "4.3.2","5.4.0");
+	project<-"JHotDraw"
 
 	data <- read.table(paste("D:/Backup/eclipse-workspace/PACOTE/results/JARProjectCharacteristics", project, ".data", sep = ""), header=TRUE);
-	`%notin%` <- Negate(`%in%`)
-	
-	
-	if (project=="JHotDraw"	){
-		data <- subset(data, versions %notin% JHotDraw_Exclude)
-	}
-	
-	if (project=="JEdit"	){
-		data <- subset(data, versions %notin% JEdit_Exclude)
-	}
-	
 
 	unique_versions <- unique(data$versions);
 	colnames <- c("Packages", "Classes", "Attrs", "Meths", "PMeths");
@@ -32,19 +17,10 @@
 		result1[version_, "Meths"] <- sum(vdata$meths);
 		result1[version_, "PMeths"] <- sum(vdata$pmeth);
 	}
-	#result1
+	result1
 
 
 	data <- read.table(paste("D:/Backup/eclipse-workspace/PACOTE/results/ODEMProjectCharacteristics", project, ".data", sep = ""), header=TRUE);
-	
-	if (project=="JHotDraw"	){
-		data <- subset(data, versions %notin% JHotDraw_Exclude)
-	}
-	
-	if (project=="JEdit"	){
-		data <- subset(data, versions %notin% JEdit_Exclude)
-	}
-	
 	versions <- sort(unique(data$version));
 
 colnames <- c("CBO", "AFF", "EFF", "LCOM", "MQ", "dependencyCount" );
@@ -62,8 +38,18 @@ for (version_ in versions)
 	result2[version_, "dependencyCount"] <- round(mean(vdata$dependencyCount),2);
 }
 
+result2
+
+#result = data.frame(result1, "dep_class"=round(result2[,6]/result1[,2],2))
+	result = data.frame(result1, "dependencyCount"=round(result2[,6],2))
+
+
 #result2
 
-result = data.frame(result1, "dep_class"=round(result2[,6]/result1[,2],2))
+	#result = data.frame(result1, "dep_class"=round(result2[,6]/result1[,2],2))
 
-write.csv(round(cor(result,  method = "spearman"),2),   file=paste("D:/Backup/eclipse-workspace/PACOTE/results/size_complexity_spearman", project, ".csv", sep = ""))
+	result = data.frame(result1, "dependencyCount"=round(result2[,6],2))
+
+
+write.csv(round(cor(result,  method = "spearman"),2),   file=paste("D:/Backup/eclipse-workspace/PACOTE/data/table/size_complexity_spearman", project, ".csv", sep = ""))
+
